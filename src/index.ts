9386c47769cc6d6e2eeb34d65ba17fcb96cd76d6
Minh Nguyen <ngmikeng@gmail.com>
2021-03-30 00:18:51 +0700

    Handle request by chunks

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import TimedQueue from './timedQueue';
 import ProcessFile from './processFile';
 
 const FILE_PATH = process.argv[2];
+const MAX_ELEMENTS = 50;
 
 if (!FILE_PATH) {
   throw new Error('Input file .csv not found');
@@ -23,24 +24,43 @@ const main = async () => {
   const newData = data.map(item => {
     return {...item, destAddr: `${item.wardName}, ${item.districtName}, Ho Chi Minh City`}
   });
-  // call google api distance matrix
   console.log(`Processing calcuate distance...`);
-  const distanceResult = await distanceCalc.simpleDistance({
-    fromAddr: 'LOTTE Mart Quận 7, Đường Nguyễn Hữu Thọ, Tân Hưng, District 7, Ho Chi Minh City',
-    toAddrs: newData.map(item => item.destAddr)
+  // calc by chunk because limit quota 100 elements per request
+  let startIndex = 0;
+  let curChunk = newData.slice(startIndex, MAX_ELEMENTS);
+  const chunks = [curChunk];
+  while (curChunk.length) {
+    startIndex = startIndex + MAX_ELEMENTS;
+    const lastIndex = startIndex + MAX_ELEMENTS;
+    curChunk = newData.slice(startIndex, lastIndex);
+    if (curChunk.length) {
+      chunks.push(curChunk);
+    }
+  }
+  // call google api distance matrix
+  let outData = [];
+  const promises = chunks.map(async chunkData => {
+    const distanceResult = await distanceCalc.simpleDistance({
+      fromAddr: 'LOTTE Mart Quận 7, Đường Nguyễn Hữu Thọ, Tân Hưng, District 7, Ho Chi Minh City',
+      toAddrs: chunkData.map(item => item.destAddr)
+    });
+    const destAddrs = distanceResult.destination_addresses;
+    const originAddr = distanceResult.origin_addresses[0];
+    const distElements = distanceResult.rows[0].elements;
+    console.log('Distance Result', JSON.stringify(distanceResult));
+    // write new file csv
+    const chunkNewData = chunkData.map((item, index) => {
+      return {
+        ...item,
+        ggOriginAddr: originAddr, ggDestAddr: destAddrs[index],
+        ggDistance: distElements[index].distance.text, ggDuration: distElements[index].duration.text
+      };
+    });
+    outData = outData.concat(chunkNewData);
+    return chunkNewData;
   });
-  const destAddrs = distanceResult.destination_addresses;
-  const originAddr = distanceResult.origin_addresses[0];
-  const distElements = distanceResult.rows[0].elements;
-  console.log('Distance Result', JSON.stringify(distanceResult));
-  // write new file csv
-  const outData = newData.map((item, index) => {
-    return {
-      ...item,
-      ggOriginAddr: originAddr, ggDestAddr: destAddrs[index],
-      ggDistance: distElements[index].distance.text, ggDuration: distElements[index].duration.text
-    };
-  });
+  await Promise.all(promises);
+  // handle write file
   console.log('Out data item: ', outData[0]);
   const header = Object.keys(outData[0]).map(value => {
     return { id: value, title: value }
